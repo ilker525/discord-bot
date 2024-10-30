cb00ada5c623ec9511cf53a80ea714a405a0e12c
ilker525 <ilk.darkflower@gmail.com>
2024-10-30 20:24:10 +0300

    Add files via upload

---
+++ ayarlar.py
@@ -0,0 +1,6 @@
+#settings.py in original
+
+ayarlar = {
+    "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
+    "TOKEN": "MTI5NjgzNDc4MjA1OTI5ODkwNg.GdwIEP.71ehb4s2OpDrtR_hRBJC5pFfFAuDgkNkbtIfa8"
+}
